005eb3345ce471c0e347ffc3695cbed2ba2030db
akashJainAJ11 <akashjain100200@gmail.com>
2023-08-27 00:35:03 +0530

    basic folder 01 is added.

---
+++ 01/hello.js
@@ -0,0 +1,137 @@
+// console.log("Akash");
+
+// let score = "33abc";
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueNumber = Number(score);
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// ****************************************************
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log("1"+2+"2");
+// console.log(1+"2"+2);
+
+// ********************************************************
+// const accountId = 16523;
+// let accountEmail = "akash@gmail.com"
+// var accountPassword = "12345";
+// accountCity = "jaipur";
+// let accountState;
+// console.table([accountId,accountEmail,accountPassword, accountCity, accountState]);
+
+
+
+// **********************************************************
+
+//stack, heap
+
+// let userOne = {
+//     email: "user@gmail.com",
+//     age: 25
+// }
+
+// let userTwo = userOne;
+
+// userTwo.email = "new@gmail.com";
+
+// console.log(userOne.email);
+// console.log(userTwo.email);
+
+// // string
+// const name = "akash";
+// const repoCount = 50;
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}.`);
+
+// const gameName = new String('akash-aj-com');
+// console.log(gameName);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('k'));
+
+// const newString = gameName.substring(0,4);
+
+// console.log(newString);
+
+// const anotherString = gameName.slice(-1,-2);
+// console.log(anotherString);
+
+// const newStringOne = "   akash   ";
+// console.log(newStringOne.trim());
+
+// const url = "https://akash.com/akash%20jain";
+// console.log(url.replace('%20', '-'));
+// console.log(url.includes('%20'));
+// console.log(url.includes('akash'));
+
+// console.log(gameName.split('-'));
+
+
+// ******************************************************
+
+// Number 
+// const score = 75;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toString());
+// console.log(balance.toFixed(2));
+
+// const otherNumber = 23.8966;
+// console.log(otherNumber.toPrecision(2));
+
+// const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// -**********************************************
+
+// maths
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 3, 6, 8));
+// console.log(Math.max(4, 5, 3, 8));
+// console.log(Math.round(Math.random()*10)+1);
+
+// const min = 10;
+// const max = 20;
+
+// console.log(Math.floor(Math.random()*(max-min+1)) + min);
+
+//Date
+
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toTimeString());
+// console.log(myDate.toLocaleTimeString())
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23);
+// console.log(myCreatedDate.toDateString());
+
+// let myTimeStamp = Date.now();
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+
+
+// let newDate = new Date();
+// console.log(newDate);
+// console.log(newDate.getMonth());
+// console.log(newDate.getDate());
---
+++ 02/app.js
@@ -0,0 +1,4 @@
+// array
+
+const myarr = [0, 1, 2, 5, "akash"];
+console.log(myarr);
