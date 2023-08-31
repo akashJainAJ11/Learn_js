4a1b293d788d876131288dcd62b50f75dd6ee01e
akashJainAJ11 <akashjain100200@gmail.com>
2023-08-31 23:18:21 +0530

    object02.js file added

---
+++ 02/object02.js
@@ -0,0 +1,15 @@
+const course = {
+    coursename: "js",
+    price: "999",
+    courseInstructer: "akash"
+}
+
+const {courseInstructer: instructer} = course
+// console.log(courseInstructer);
+console.log(instructer);
+
+// {
+//     "name": "Akash",
+//     "price": "free",
+//     "coursename": "js"
+// }
